7bcd315db99fcd8048840751351fc9f6527c35b4
Victor DEGLIAME <victor.degliame@gmail.com>
2025-08-25 18:32:09 +0200

    chore: Remove unnecessary type assertion

--- src/comment-handlers/error-comment-handler.ts
+++ src/comment-handlers/error-comment-handler.ts
@@ -45,7 +45,7 @@ function generateGrafanaLogsUrl(): string {
   const queryString = Object.entries(params)
     .map(
       ([key, value]) =>
-        `${encodeURIComponent(key)}=${encodeURIComponent(value as string)}`
+        `${encodeURIComponent(key)}=${encodeURIComponent(value)}`
     )
     .join('&')
 
