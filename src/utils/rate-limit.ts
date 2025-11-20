e78b9396ef28247b5ca4012584458d131278a8d0
Jo <devthejo@protonmail.com>
2025-11-20 11:16:47 +0100

    Update src/utils/rate-limit.ts
    Co-authored-by: RevuDev <socialgroovybot+revu-bot-dev@fabrique.social.gouv.fr>

--- src/utils/rate-limit.ts
+++ src/utils/rate-limit.ts
@@ -1,4 +1,4 @@
-import * as IORedis from 'ioredis'
+import Redis from 'ioredis'
 import { logSystemWarning } from './logger.ts'
 
 type RedisClient = // eslint-disable-next-line @typescript-eslint/no-explicit-any
